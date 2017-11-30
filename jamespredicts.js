ee86775d384835332436863ad5280865cebb016a
Paul Borawski <paulborawski14@gmail.com>
2017-11-29 18:50:41 -0800

    server gitignore

---
+++ jamespredicts.js
@@ -0,0 +1,20 @@
+var port = 8764;
+
+
+
+var app = require('express')();
+
+app.use(require('compression')());
+app.use(require('body-parser').json());
+app.use(require('body-parser').urlencoded({ extended:true }));
+/*app.use(require('stylus').middleware(require('path').join(__dirname, 'public')));*/
+app.use(require('express').static('public'));
+
+app.get('/', function (req, res) {
+  res.send('iamnawa');
+});
+
+
+require('http').createServer(app).listen(port,function(){
+  console.log('server is running you beeezy, on port', port)
+});
